269905433ba032fd72c6c8cca2526de346a41dfc
ibrahimalaqoul <alaqoulibrahim@gmail.com>
2022-05-29 23:24:16 +0300

    chal36done

--- javascript/Graphs/graph.test.js
+++ javascript/Graphs/graph.test.js
@@ -65,4 +65,23 @@ describe('Graph', () => {
         myGraph.removeNode(1);
         expect(myGraph.size()).toBe(2);
     })
+    it('should be able to BreadthFirst search', () => {
+        myGraph.addNode(1);
+        myGraph.addNode(2);
+        myGraph.addNode(3);  
+        myGraph.addNode(4);
+        myGraph.addNode(5);
+        myGraph.addEdge(1, 2, 5);
+        myGraph.addEdge(1, 3, 5);
+        myGraph.addEdge(2, 3, 5);
+        myGraph.addEdge(2, 4, 5);
+        myGraph.addEdge(4, 5, 5);
+        myGraph.addEdge(5, 3, 5);
+        expect(myGraph.BreadthFirst(1)).toEqual(new Set([1, 2, 3, 4, 5]));
+        let myGraph2 = new graph();
+        expect(myGraph2.BreadthFirst('')).toEqual('The Graph is empty');
+        myGraph2.addNode();
+        expect(myGraph2.BreadthFirst('')).toEqual(null);
+
+    })
 })
---
+++ javascript/Graphs/readme.BFS.md
@@ -0,0 +1,10 @@
+# Challenge Summary
+The challange is to write a method for graph class that do the breadth first way of searching which take a node and  return A collection of nodes in the order they were visited.
+
+## Whiteboard Process
+![](./chal36img.PNG)
+## Approach & Efficiency
+The approach is to use a queue to keep track of the nodes that are visited.
+i used while loop to keep track of the nodes that are visited.
+## Solution
+graph.test.js
